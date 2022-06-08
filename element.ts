a698ca408095297cb5b76cc92177882126f9c349
Joseph Bacal <bacalj@gmail.com>
2022-06-08 18:52:18 -0400

    remove comment

--- element.ts
+++ element.ts
@@ -1,6 +1,5 @@
 import { MapLocation } from "./theTypes";
 
-// TODO: how would I guarantee that an element cannot be instantiated unless <GameElement> matches Obstacle, or Potion? How can I access the value of <GameElement> in my constructor or anywhere else?
 export class Element<SomeType> implements MapLocation {
     id: string
     x: number
