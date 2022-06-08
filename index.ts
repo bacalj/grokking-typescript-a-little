8b31d33df64b06666f527ab9a7de3a014c27bec6
Joseph Bacal <bacalj@gmail.com>
2022-06-08 17:18:22 -0400

    explanation

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { frogData, wallData, potionData, carData } from "./myData"
 
 let theFrog = new Player('frog1', frogData.info, frogData.location)
 let theWall = new Element<GameElement>('wall1', wallData.info, wallData.location )
+/* this will not compile, because carData is not shaped like a GameElement */
 // let thePotion = new Element<GameElement>('potion1', carData.info, potionData.location )
 let thePotion = new Element<GameElement>('potion1', potionData.info, potionData.location )
 
