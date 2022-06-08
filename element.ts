35ac184aa119c0ba3d1f42c5395035f888cf608d
Joseph Bacal <bacalj@gmail.com>
2022-06-08 16:11:23 -0400

    arbitrarty

--- element.ts
+++ element.ts
@@ -1,13 +1,13 @@
-import { MapLocation, Potion, Obstacle } from "./theTypes";
+import { MapLocation, GameElement } from "./theTypes";
 
 // TODO: how would I guarantee that an element cannot be instantiated unless <GameElement> matches Obstacle, or Potion? How can I access the value of <GameElement> in my constructor or anywhere else?
-export class Element<GameElemento> implements MapLocation {
+export class Element<GameElement> implements MapLocation {
     id: string
     x: number
     y: number
-    details: GameElemento
+    details: GameElement
 
-    constructor(id: string, details: GameElemento, initialLocation: MapLocation ){
+    constructor(id: string, details: GameElement, initialLocation: MapLocation ){
         this.id = id
         this.x = initialLocation.x
         this.y = initialLocation.y
